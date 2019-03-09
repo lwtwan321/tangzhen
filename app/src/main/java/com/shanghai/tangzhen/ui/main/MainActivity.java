a513e9f2767a5f8d27cfbae3da1d6099bec83171
MS-20170609BEYD\Administrator <jiahui.ding@sandbox3.cn>
2019-03-09 15:38:57 +0800

    init comiti

--- app/src/main/java/com/shanghai/tangzhen/ui/main/MainActivity.java
+++ app/src/main/java/com/shanghai/tangzhen/ui/main/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tv.setText(stringFromJNI());
+
+        
     }
 
     /**
